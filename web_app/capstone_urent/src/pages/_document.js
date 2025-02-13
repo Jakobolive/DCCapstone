2bfe65d0c01560f135c2ebf98357ef7f606199f1
CurtisHemm <146120781+CurtisHemm@users.noreply.github.com>
2025-02-12 19:06:19 -0500

    added all pages into folders
    added all pages into folder

--- web_app/capstone_urent/src/pages/_document.js
+++ web_app/capstone_urent/src/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Footer from '../components/footer/footer';
+import Header from '../components/header/header';
 
 export default function Document() {
   return (
